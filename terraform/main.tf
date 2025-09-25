42bbfea70d30e9f476232a24002a76fdb40aa3bd
Ahmed Samy <ahmedsami2302@gmail.com>
2025-09-25 15:52:48 +0300

    edit .gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "your-gcp-project-id"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {} # Needed to assign a public IP
+  }
+
+  metadata = {
+    ssh-keys = "your-username:${file("~/.ssh/id_rsa.pub")}"
+  }
+}

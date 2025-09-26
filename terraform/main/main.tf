78de184631abc72cb1e329f75f2ab6b4b33ebd48
Ahmed Samy <ahmedsami2302@gmail.com>
2025-09-26 10:33:21 +0300

    fix cloud storage module path

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,7 +1,7 @@
 # Cloud storage module
 
 module "cloud_storage_bucket" {
-  source      = "./modules/cloud_storage"
+  source      = "../modules/cloud_storage"
   env         = var.env
   region      = var.region
 }
